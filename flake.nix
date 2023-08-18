c25d266b7a9b91359b80570ea27be6a75905a26c
Carson Webster <carsonwebster@hotmail.com>
2023-08-18 16:39:30 -0700

    Home entry fixed?

--- flake.nix
+++ flake.nix
@@ -31,4 +31,16 @@
         };
       };
     };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # FIXME replace with your username@hostname
+      "carson@Terra" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main home-manager configuration file <
+        modules = [ ./nixos/home.nix ];
+      };
+    };
 }
