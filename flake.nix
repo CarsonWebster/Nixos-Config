6e91783de442f5ca27c7d932a65ae485e0a2777e
Carson Webster <carsonwebster@hotmail.com>
2023-08-18 16:06:30 -0700

    Just inital config

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My NixOS System Configuration Flake";
+
+  inputs = {
+    nixpkgs.url = "github.com/nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github.com/nixos/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      nixosConfigurations = {
+        Terra = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit system; };
+          modules = [
+            ./hosts/terra/configuration.nix
+          ];
+        };
+      };
+    };
+}
