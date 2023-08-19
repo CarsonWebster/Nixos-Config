0a76792bf3a65356d6d3173b4c5e0b6598e2829d
Carson Webster <carsonwebster@hotmail.com>
2023-08-18 18:13:15 -0700

    Tailscale and lazyvim install

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -105,9 +105,12 @@
     isNormalUser = true;
     description = "Carson Webster";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
 
       home-manager
+
+      tailscale
 
       firefox
       kate
@@ -152,6 +155,8 @@
       # rofi-wayland
     ];
   };
+  programs.zsh.enable = true;
+  services.tailscale.enable = true;
   programs.neovim.enable = true;
   programs.steam = {
     enable = true;
@@ -183,11 +188,54 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey tskey-auth-kCmakE7CNTRL-RZJwD9MkDzULWYFU9yhnyUbLUodQr33D
+    '';
+  };
+
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall = {
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    # allow you to SSH in over the public internet
+    allowedTCPPorts = [ 22 ];
+  };
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- nixos/home.nix
+++ nixos/home.nix
@@ -34,6 +34,8 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+
+    lazygit
   ];
 
   programs.git = {
