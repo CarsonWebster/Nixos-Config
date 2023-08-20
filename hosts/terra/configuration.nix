dcc4c48a0432571e26ff0fa4bd1a66e75dd77c48
Carson Webster <carsonwebster@hotmail.com>
2023-08-20 14:42:01 -0700

    Hyprland from flake

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -64,9 +64,13 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Enable hyprland
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.enable = true;
+  };
   # programs.hyprland.xwayland.enable = true;
-  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # services.gnome.gnome-remote-desktop.enable = true;
 
